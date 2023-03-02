477033bb63291148b48e3032a47f50becf4fa7e4
cquilinderino <cquilinderino@gmail.com>
2023-03-02 18:47:22 +1100

    User interface implemented to main method.

--- Part 8 - Recap/Liquid/Liquid/src/Main.java
+++ Part 8 - Recap/Liquid/Liquid/src/Main.java
@@ -2,18 +2,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Container first = new Container();
+        Container second = new Container();
 
-        Container container = new Container();
-        System.out.println(container);
+        System.out.println("Commands:");
+        System.out.println("- 'add x' adds x to the first container");
+        System.out.println("- 'move x' removes x from the first container and adds it to the second");
+        System.out.println("- 'remove x' removes x from the second container");
+        System.out.println("- 'quit' exits the program");
 
-        container.add(50);
-        System.out.println(container);
-        System.out.println(container.contains());
+        while(true) {
+            System.out.println("First container: " + first);
+            System.out.println("Second container: " + second);
 
-        container.remove(60);
-        System.out.println(container);
+            String input = scanner.nextLine();
 
-        container.add(200);
-        System.out.println(container);
+            if (input.equals("quit")){
+                break;
+            }
+
+            String[] commands = input.split(" ");
+            input = commands[0];
+            int amount = Integer.valueOf(commands[1]);
+
+            if (input.equals("add")) {
+                first.add(amount);
+            } else if (input.equals("move")) {
+                first.remove(amount);
+                second.add(amount);
+            } else if (input.equals("remove")) {
+                second.remove(amount);
+            }
+        }
     }
 }
