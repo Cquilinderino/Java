5a636782559709de602ffd24e03866bb5989952f
cquilinderino <cquilinderino@gmail.com>
2023-03-03 16:56:55 +1100

    Hashmap practice using loops as an interface

--- Part 8 - Recap/Nicknames/src/Main.java
+++ Part 8 - Recap/Nicknames/src/Main.java
@@ -1,7 +1,33 @@
 
 import java.util.HashMap;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test commit");
+        HashMap<String, String> nicknames = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Type 'exit' to terminate logging mode");
+
+            System.out.print("Enter id of value: ");
+            String id = scanner.nextLine();
+
+            if (id.equals("exit")) {
+                break;
+            }
+
+            System.out.print("Enter value: ");
+            String value = scanner.nextLine();
+
+            nicknames.put(id, value);
+        }
+
+        while (true) {
+            System.out.println("Please enter id:");
+            String id = scanner.nextLine();
+
+            System.out.println(nicknames.get(id));
+        }
+
     }
 }
