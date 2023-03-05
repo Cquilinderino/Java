e586aaa7c91c540bd507b8d15af765b4e05559d0
cquilinderino <cquilinderino@gmail.com>
2023-03-05 23:57:36 +1100

    Hashmap looping

---
+++ Part 8 - Recap/PrintMeMyHashMap/src/Program.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Program {
+
+
+    public static void printKeys(HashMap<String, String> hashmap) {
+
+
+        for(String keys: hashmap) {
+            System.out.println(keys);
+        }
+    }
+    public static void printKeysWhere(HashMap<String, String> hashmap) {
+
+    }
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text) {
+
+    }
+}
